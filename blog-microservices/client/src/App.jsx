29b3ce2519acf1a7d8a1c6e1234fa006d69ade45
nuhptr <nugrahaadi733@gmail.com>
2023-12-08 22:48:44 +0700

    src/update code

--- blog-microservices/client/src/App.jsx
+++ blog-microservices/client/src/App.jsx
@@ -1,3 +1,14 @@
+import PostCreate from "./components/post-create"
+import PostList from "./components/post-list"
+
 export default function App() {
-   return <div>Blog App</div>
+   return (
+      <div className="container">
+         <h1>Create Post</h1>
+         <PostCreate />
+         <hr />
+         <h1>Posts</h1>
+         <PostList />
+      </div>
+   )
 }
